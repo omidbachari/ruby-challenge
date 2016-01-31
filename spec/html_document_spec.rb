722ff5c1505ca6b25fca547cca48dbf8a247d126
Omid Bachari <obachari@jobcase.com>
2016-01-31 15:41:54 -0500

    expect HtmlDocument to return content

--- spec/html_document_spec.rb
+++ spec/html_document_spec.rb
@@ -1,4 +1,15 @@
 require_relative "../lib/html_document.rb"
 
 describe HtmlDocument do
+  describe 'meta_keywords' do
+    let(:doc) { HtmlDocument.new({}) }
+    let(:content) { 'lifestyle' }
+
+    context 'when cached @meta_keywords' do
+      it 'returns cached meta_keywords content' do
+        doc.instance_variable_set(:@meta_keywords, content)
+        expect(doc.meta_keywords).to be content
+      end
+    end
+  end
 end

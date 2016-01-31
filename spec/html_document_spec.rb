2822e600885a7b175d536d535c0cadea2fb65fed
Omid Bachari <obachari@jobcase.com>
2016-01-31 15:42:44 -0500

    expect array of when meta tag exists

--- spec/html_document_spec.rb
+++ spec/html_document_spec.rb
@@ -4,11 +4,28 @@ describe HtmlDocument do
   describe 'meta_keywords' do
     let(:doc) { HtmlDocument.new({}) }
     let(:content) { 'lifestyle' }
+    let(:content_collection) { 'dog cat people' }
 
     context 'when cached @meta_keywords' do
       it 'returns cached meta_keywords content' do
         doc.instance_variable_set(:@meta_keywords, content)
         expect(doc.meta_keywords).to be content
+      end
+    end
+
+    context 'when no cached @meta_keywords, with existing meta tag' do
+      it 'returns array of content' do
+        class ContentMock
+        end
+
+        parser = double(parser)
+        content_mock = ContentMock.new
+
+        allow(content_mock).to receive(:attribute) { content_collection }
+        allow(parser).to receive(:get_meta_tag) { content_mock }
+
+        expect(HtmlDocument.new(parser).meta_keywords).to eq [content_collection]
+
       end
     end
   end
